a18d3fcad5ec96f7d7460139b510d65b05e64c04
Ursula <ursula.messick.95@my.csun.edu>
2013-11-29 20:30:01 -0800

    Add Verdict Controller

---
+++ app/controllers/VerdictController.php
@@ -0,0 +1,26 @@
+<?php
+
+class VerdictController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return "Nothing Yet";
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return "nothing yet";
+	}
+}
