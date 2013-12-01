790c77340668bd7d5c8e7744246898f2d845ca26
Ursula Messick <ursula.messick.95@my.csun.edu>
2013-11-30 16:40:04 -0800

    Add Count by Sensor to Verdict Record
    Display the number of records for each sensor

--- app/controllers/VerdictController.php
+++ app/controllers/VerdictController.php
@@ -20,7 +20,30 @@ class VerdictController extends \BaseController {
 	 * @return Response
 	 */
 	public function show($id)
-	{
-		return "nothing yet";
+	{	
+		$accel_records = XYZRecord::where("session_id", "123")->where("sensor_id","1")->get();
+		$light_records = XYZRecord::where("session_id", "123")->where("sensor_id","2")->get();
+		
+		$dataArray[] = array(
+			'sensor_id' => 1,
+			'sensor_name' => "Accellerometer",
+			'record_count' => sizeof($accel_records->toArray())
+		);
+
+		$dataArray[] = array(
+			'sensor_id' => 2,
+			'sensor_name' => "Light",
+			'record_count' => sizeof($light_records->toArray())
+		);
+
+		/* Prepare response with new resource data */
+		$response = array(
+			'id' 		  => 'Success',
+			'description' => 'Data successfully retrieved',
+			'data' 		  => $dataArray,
+			'status' 	  => 200
+		);
+
+		return $response;
 	}
 }
