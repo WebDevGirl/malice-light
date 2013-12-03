c0cf4a950bbfbfd372843276e6bb290c28f42127
Ursula Messick <ursula.messick.95@my.csun.edu>
2013-12-03 14:55:18 -0800

    Create Octave script and execute
    Build a octave script from the data and send it to octave to munch on. Suppressed the the octave output so only the JSON is returned.

--- app/controllers/VerdictController.php
+++ app/controllers/VerdictController.php
@@ -21,26 +21,72 @@ class VerdictController extends \BaseController {
 	 */
 	public function show($id)
 	{	
+
+		ob_start(); //-- Start Output surpression
+
+		/* Get Data To Analyze */
 		$accel_records = XYZRecord::where("session_id", $id)->where("sensor_id","1")->get();
 		$light_records = XYZRecord::where("session_id", $id)->where("sensor_id","2")->get();
 		
-		$dataArray[] = array(
+		/* Set Time at first point */
+		$init_time = $accel_records[0]['record_float'];
+
+
+		/* Get points for x vector */
+		$x_points = "";
+		foreach($light_records as $rec) {
+			$x_points .= ($rec['record_float'] - $init_time) . ", "; 
+		}
+		$x_points = substr($x_points,0,strlen($x_points)-2); // remove tailing ,
+
+		
+		/* Get points for y vector */
+		$y_points = "";
+		foreach($accel_records as $rec) {
+			$y_points .= ($rec['record_float'] - $init_time) . ", "; 
+		}
+		$y_points = substr($y_points,0,strlen($y_points)-2); // remove tailing ,
+
+
+		/* Build Octave Script - Graph */
+		$octave_script = "
+		\nx=[${x_points}];
+		\ny=[${y_points}];
+
+		\nplot(x,y);
+  		\nprint -dpng ".public_path()."/images/file.png;";
+
+		/* Set Script Filename */
+		$octave_script_path = storage_path()."/cache/file.m";
+
+		/* Print To File */
+		touch($octave_script_path);
+		File::put($octave_script_path, $octave_script);
+
+		/* System Call - Forgive this transgression */
+		system(' octave --silent --eval "source(\''.$octave_script_path.'\');"');
+
+		ob_end_clean(); //-- End Output surpression
+
+		$dataArray['Accellerometer'] = array(
 			'sensor_id' => 1,
 			'sensor_name' => "Accellerometer",
 			'record_count' => sizeof($accel_records->toArray())
 		);
 
-		$dataArray[] = array(
+		$dataArray['Light'] = array(
 			'sensor_id' => 2,
 			'sensor_name' => "Light",
 			'record_count' => sizeof($light_records->toArray())
 		);
+
 
 		/* Prepare response with new resource data */
 		$response = array(
 			'id' 		  => 'Success',
 			'description' => 'Data successfully retrieved',
 			'data' 		  => $dataArray,
+			'url' => "http://tsar208.grid.csun.edu/malice-light/public/images/file.png",
 			'status' 	  => 200
 		);
 
