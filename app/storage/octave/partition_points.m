89b8d84357532d69531e637bd253ad77456b9e8f
Ursula Messick <ursula.messick.95@my.csun.edu>
2013-12-04 20:40:10 -0800

    Add main graph to top, don't add 'ignored' points
    Put all the points at the top for reference (takes two columns) and don't include points we are ignoring within the threshold.

--- app/storage/octave/partition_points.m
+++ app/storage/octave/partition_points.m
@@ -1,5 +1,5 @@
-% y=[0,8,0,-8,0,4,4,4,4,0,-8,0,8,0,4,2,0,0,8,0,8,2,2,2,2,2,2,0,8,4,2,0,8,-8,0,0,1,1,1,1,12,3,0,-8,1,1,1,1,1,1,1,1,1,1,1,10,1,0,1,10,0,0,10,10,0,0,10,10,4,4,4,4,4,4,4,4,4,4,4,4,10,1,-9,1,0,10,4,4,4,4,4,4,4,4,4,4,4,4,4,0,5,0,5,0,5,10,1,1,1,1,1,10,5,0,1,2,3,4,5,6,7,8,10,9,8,7,6,5,4];
-% x=[1:size(y)(2)];
+y=[0,8,0,-8,0,4,4,4,4,4,4,4,4,4,4,4,4,0,-8,0,8,0,4,2,0,0,8,0,8,2,2,2,2,2,2,2,2,2,2,2,2,0,8,4,2,0,8,-8,0,0,1,12,3,0,-8,1,10,1,0,1,10,0,0,10,10,0,0,10,10,4,4,4,4,4,4,4,4,4,4,4,4,10,1,-9,1,0,10,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,10,-2,10,-2,10];
+x=[1:size(y)(2)];
 
 function partition_points(x_points,y_points)
 
@@ -55,9 +55,11 @@ function partition_points(x_points,y_points)
 
         endif
 
-        %add to partition
-        curr_x_parts(end+1) = x_points(counter); 
-        curr_y_parts(end+1) = i; 
+        %add to partition if not ignoring
+        if (is_ignoring==0)
+            curr_x_parts(end+1) = x_points(counter); 
+            curr_y_parts(end+1) = i; 
+        endif
 
         %index coutner
         counter=counter+1;
@@ -72,12 +74,18 @@ function partition_points(x_points,y_points)
         p_num = p_num - 1
     endif   
 
-
     % Plot All the Graphcs
-    i_counter = 1;
+    graph_rows = (ceil(p_num/2))+1
+    
+    % plot all points on top
+    subplot(graph_rows,2,1:2)
+    plot(x_points,y_points)
 
-    for i = 1:p_num
-        subplot((ceil(p_num/2)),2,i_counter)
+      
+    i_counter = 1;
+    g_counter = 3;
+    for i = 1:p_num    
+        subplot(graph_rows,2,(i_counter+2))    
         plot(all_x_parts{1,i_counter},all_y_parts{1,i_counter})
         i_counter=i_counter+1
     endfor
