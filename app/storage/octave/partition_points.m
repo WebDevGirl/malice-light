52fca22be4eb772729e8b5622baf5882aab9af48
Ursula Messick <ursula.messick.95@my.csun.edu>
2013-12-04 18:28:59 -0800

    Update Octave Graphing Partition Script
    Path replaced with ===FILE_PATH_HERE=== instead of passed in function.
    
    One less partition exists if we are still in ignore mode.
    
    Add ceiling to make sure subplot has correct row numbers

--- app/storage/octave/partition_points.m
+++ app/storage/octave/partition_points.m
@@ -1,7 +1,7 @@
 % y=[0,8,0,-8,0,4,4,4,4,0,-8,0,8,0,4,2,0,0,8,0,8,2,2,2,2,2,2,0,8,4,2,0,8,-8,0,0,1,1,1,1,12,3,0,-8,1,1,1,1,1,1,1,1,1,1,1,10,1,0,1,10,0,0,10,10,0,0,10,10,4,4,4,4,4,4,4,4,4,4,4,4,10,1,-9,1,0,10,4,4,4,4,4,4,4,4,4,4,4,4,4,0,5,0,5,0,5,10,1,1,1,1,1,10,5,0,1,2,3,4,5,6,7,8,10,9,8,7,6,5,4];
 % x=[1:size(y)(2)];
 
-function partition_points(x_points,y_points,file_path)
+function partition_points(x_points,y_points)
 
     % loop_counter
     counter=1;
@@ -63,12 +63,13 @@ function partition_points(x_points,y_points,file_path)
         counter=counter+1;
     endfor
 
-    % Attach last partition if not ignoring points
     if (is_ignoring == 0)
-       
+        % Attach last partition if not ignoring points
         all_x_parts(p_num) = curr_x_parts;
         all_y_parts(p_num) = curr_y_parts;
-
+    else
+        % One less partition then counted
+        p_num = p_num - 1
     endif   
 
 
@@ -76,13 +77,13 @@ function partition_points(x_points,y_points,file_path)
     i_counter = 1;
 
     for i = 1:p_num
-        subplot((p_num/2),2,i_counter)
+        subplot((ceil(p_num/2)),2,i_counter)
         plot(all_x_parts{1,i_counter},all_y_parts{1,i_counter})
         i_counter=i_counter+1
     endfor
 
-    print -dpng file_path;
+    print -dpng '===FILE_PATH_HERE===';
 
 endfunction
 
-% partition_points(x,y, '/www/var/file.png');
+partition_points(x,y);
